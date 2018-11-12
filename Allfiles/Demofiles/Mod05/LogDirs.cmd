4f1cdfef6c82c7fbe08db1fc28c7d4de610df843
Andy <42963410+AndyFogg@users.noreply.github.com>
2018-11-12 10:01:57 +0000

    Test pass complete

---
+++ Allfiles/Demofiles/Mod05/LogDirs.cmd
@@ -0,0 +1,8 @@
+MD E:\Logfiles
+MD E:\Logfiles\ambari-agent
+MD E:\Logfiles\ambari-server
+MD E:\Logfiles\hadoop
+MD E:\Logfiles\hdinsight-agent
+MD E:\Logfiles\hdinsight-startupagent
+MD E:\Logfiles\syslog
+MD E:\Logfiles\yarn
